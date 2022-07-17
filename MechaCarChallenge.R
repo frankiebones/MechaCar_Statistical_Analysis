2f96bc618716c92b6f9698960e3a97ae430fb95c
Frank Bucalo <frankiebucalo@gmail.com>
2022-07-17 08:44:41 -0400

    update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,9 @@ mecha_mpg <- read.csv("C:/Users/Frank Bucalo/Desktop/classwork/MechaCar_Statisti
 mpg_matrix <- lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_mpg) #generate multiple linear regression model
 
 summary(mpg_matrix) #summarize (r-squared: 0.7149, p-value: 5.35e-11)
+
+coil_table <- read.csv("C:/Users/Frank Bucalo/Desktop/classwork/MechaCar_Statistical_Analysis/Suspension_Coil.csv",check.names=F,stringsAsFactors=F) # import data
+
+total_summary <- coil_table %>% summarize(Manufacturing_Lot="All",Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #generate summary statistics table for PSI
+
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') #generate summary statistics table for PSI from each Manufacturing Lot
